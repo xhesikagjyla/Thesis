918e92446e9e1866cec8985aef4be6d3086c9a41
Xhesika Gjyla <Xhesika.Gjyla@fti.edu.al>
2024-11-18 20:08:05 +0100

    first commit

---
+++ live-messenger/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\MessengerController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('messenger', [MessengerController::class,'index'])->name('home');
+    Route::post('profile', [UserProfileController::class, 'update'])->name('profile.update');
+    //search route
+    Route::get('messenger/search', [MessengerController::class,'search'])->name('messenger.search');
+    //fetch user by id
+    Route::get('messenger/id-info', [MessengerController::class,'fetchIdInfo'])->name('messenger.id-info');
+    //send message
+    Route::post('messenger/send-message', [MessengerController::class, 'sendMessage'])->name('messenger.send-message');
+     //fetch message
+    Route::get('messenger/fetch-messages', [MessengerController::class, 'fetchMessages'])->name('messenger.fetch-messages');
+    // //fetch contacts
+    Route::get('messenger/fetch-contacts', [MessengerController::class, 'fetchContacts'])->name('messenger.fetch-contacts');
+     // update contacts
+    Route::get('messenger/update-contact-item', [MessengerController::class, 'updateContactItem'])->name('messenger.update-contact-item');
+    // seen
+    Route::post('messenger/make-seen', [MessengerController::class, 'makeSeen'])->name('messenger.make-seen');
+    // favorite routes
+    Route::post('messenger/favorite', [MessengerController::class, 'favorite'])->name('messenger.favorite');
+    // fetch favorite contacts
+    Route::get('messenger/fetch-favorite', [MessengerController::class, 'fetchFavoritesList'])->name('messenger.fetch-favorite');
+    // delete messages
+    Route::delete('messenger/delete-message', [MessengerController::class, 'deleteMessage'])->name('messenger.delete-message');
+
+});
